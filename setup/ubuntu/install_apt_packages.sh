31b93e113787c9825bc17ca9cb2a2b558a68a1e0
Nobuchika Tanaka <foundfox.7799@gmail.com>
2020-12-26 09:59:11 +0900

    Initial commit

---
+++ setup/ubuntu/install_apt_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Vcs
+sudo apt install -y git
+
+# Editor
+sudo apt install -y neovim
---
+++ setup/ubuntu/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo ~/dotfiles/setup/ubuntu/update_apt_packages.sh
+sudo ~/dotfiles/setup/ubuntu/upgrade_apt_packages.sh 
+sudo ~/dotfiles/setup/ubuntu/install_apt_packages.sh
